e93e167a2ae8be9f7673d3ce37d43aa954cf6138
meenakshi1811 <136489830+meenakshi1811@users.noreply.github.com>
2026-06-04 12:52:03 +0530

    Enlarge header logo

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,17 +1,13 @@
 @props(['class' => 'logo-img'])
 
-<a href="{{ route('home') }}" {{ $attributes->merge(['class' => 'navbar-brand logo-link d-flex align-items-center m-0 p-0 text-decoration-none']) }}>
+<a href="{{ route('home') }}" aria-label="Hillnest home" {{ $attributes->merge(['class' => 'navbar-brand logo-link d-flex align-items-center m-0 p-0 text-decoration-none']) }}>
     <img
         src="{{ hillnest_logo() }}"
         alt="Hillnest logo"
         class="{{ $class }}"
-        width="184"
-        height="184"
+        width="228"
+        height="228"
         loading="eager"
         onerror="this.onerror=null; this.src='{{ asset('images/logo.svg') }}';"
     >
-    <span class="d-none d-md-block lh-1">
-        <span class="d-block brand-title">Hillnest</span>
-        <span class="d-block brand-subtitle">Pure Bilona Ghee</span>
-    </span>
 </a>
